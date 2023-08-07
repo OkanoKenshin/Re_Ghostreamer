bfa061ed0b8bfa0296a390a8ee48aa98960b9db5
tayano0106 <ttc22c1020035@edu.tech.ac.jp>
2023-08-07 21:09:36 +0900

    MStreamerCollisionCheckerの修正

--- Assets/Menber/Tayano/Scripts/AttackAction/StreamerCollisionChecker.cs
+++ Assets/Menber/Tayano/Scripts/AttackAction/StreamerCollisionChecker.cs
@@ -9,9 +9,31 @@ public class StreamerCollisionChecker : MonoBehaviour
     GameObject GhAttackIntervalContorolObject;
 
     GhAttackActionIntervalContorol _ghAttackIntervalContorol;
+
+    [SerializeField]
+    private const int layerWithStreamer = 8;
+
+    [SerializeField]
+    private const int layerWithGhost = 10;
+
+    [SerializeField]
+    private  const int layerWithManipulativeObject = 20;
+
+    //"AttackHitDetection"の参照作成
+    [SerializeField]
+    GameObject AttachedAttackHitDetection;
+    AttackHitDetection _attackHitDetection;
+
+    /*
+    //"IdentifyTouchedObjects"の参照作成
+    [SerializeField]
+    GameObject AttachedIdentifyTouchedObjects;
+    IdentifyTouchedObjects _identifyTouchedObjects;
+    */
     // Start is called before the first frame update
     void Awake()
     {
+
         #region GhAttackIntervalContorolのインスタンス取得とNullチェック(Awakeメソッドに記述)
         if (GhAttackIntervalContorolObject != null)
         {
@@ -30,6 +52,44 @@ public class StreamerCollisionChecker : MonoBehaviour
             Debug.Log("GhAttackIntervalContorolObjectは取得されていません。");
         }
         #endregion
+
+        #region AttackHitDetectionのNullチェック
+        if (AttachedAttackHitDetection != null)
+        {
+            _attackHitDetection = GetComponent<AttackHitDetection>();
+            if (_attackHitDetection != null)
+            {
+                Debug.Log("「AttackHitDetection」は正常に取得されています。");
+            }
+            else
+            {
+                Debug.Log("「AttachedAttackHitDetection」は取得されていますが、 「AttackHitDetection」の取得に失敗しています。");
+            }
+        }
+        else
+        {
+            Debug.Log("「AttachedAttackHitDetection」は取得されていません。");
+        }
+        #endregion
+
+        #region IdentifyTouchedObjectsのNullチェック
+        //if (AttachedIdentifyTouchedObjects != null)
+        //{
+        //    _identifyTouchedObjects = GetComponent<IdentifyTouchedObjects>();
+        //    if (_identifyTouchedObjects != null)
+        //    {
+        //        Debug.Log("「IdentifyTouchedObjects」は正常に取得されています。");
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("「AttachedIdentifyTouchedObjects」はアタッチされていますが、 「IdentifyTouchedObjects」の取得に失敗しています。");
+        //    }
+        //}
+        //else
+        //{
+        //    Debug.Log("「AttachedIdentifyTouchedObjects」はアタッチされていません。");
+        //}
+        #endregion
     }
 
     // Update is called once per frame
@@ -37,15 +97,37 @@ public class StreamerCollisionChecker : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter(Collision hitInfo)
+  /*  private void OnCollisionEnter(Collision hitInfo)
     //hit情報を"hitInfo"に格納
     {
-        int hitObjectlayer = hitInfo.gameObject.layer;
-        //hitInfoからヒットObjectのレイヤー情報を引き出し、hitObjectLayerに代入。
-        //if (hitObjectlayer == streamerLayerNum)
-        //{
-        //    attackHitTheStreamer = true;
-        //}
-    }
+        int layerToHit = hitInfo.gameObject.layer;
+        //hitInfoからヒットObjectのレイヤー情報を引き出し、layerToHitに代入。
+
+        Collider myCollider = GetComponent<Collider>();
+        int layerOfThis = myCollider.gameObject.layer;
+        //このscriptがアタッチされてるGameObjectについてるlayerを取得
+
+        switch (layerOfThis)
+        //このscriptがアタッチされてるGameObjectが何かを判定
+        {
+            case layerWithGhost:
+                //Ghostにこのscriptがアタッチされている場合
+                if (layerWithStreamer == layerToHit)
+                //ヒットした対象がStreamerか判定
+                {
+                    _attackHitDetection.attackHitTheStreamer = true;
+                }
+                break;
+
+            case layerWithManipulativeObject:
+                //操作可能Objectにこのscriptがアタッチされている場合
+                if (layerWithStreamer == layerToHit)
+                //Streamerが触れたObjectのTag情報を
+                {
+                    _identifyTouchedObjects.touchedObject = this.gameObject.tag;
+                }
+                break;
+        }
+    }*/
 
 }
