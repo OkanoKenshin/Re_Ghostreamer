f11f079b191b4e1cf7e84758c7b3e236625c7da8
kei0527 <t.kei0527@gmail.com>
2023-08-12 21:52:58 +0900

    途中経過　アニメーションのところでエラーあり

---
+++ Assets/Menber/Tsuchiya/Scripts/CollectTheIRCamera.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+/*public class CollectTheIRCamera : MonoBehaviour
+{
+    [SerializeField] private float waitingTime = 5.0f;
+    private Coroutine coroutineForThisClass;
+    private bool inputOn;
+
+    public void MCollectTheIRCamera()
+    {
+        coroutineForThisClass = StartCoroutine(WaitForCameraCollection());
+    }
+
+    private IEnumerator WaitForCameraCollection()
+    {
+        Debug.Log("カメラ回収アニメーションを再生");
+
+        yield return new WaitForSeconds(waitingTime);
+
+        Debug.Log("カメラ回収完了");
+    }
+
+    public void MAbortCoroutine()
+    {
+        if (coroutineForThisClass != null)
+        {
+            StopCoroutine(coroutineForThisClass);
+        }
+    }
+}
+*/
