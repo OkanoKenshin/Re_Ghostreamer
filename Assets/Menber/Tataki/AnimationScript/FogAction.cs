297ebca5af3ee6a34f705c255fa7210cefc0d3b2
tataki <107167193+haburin@users.noreply.github.com>
2023-08-26 09:26:01 +0900

    Fogアニメーションスクリプト

--- Assets/Menber/Tataki/AnimationScript/FogAction.cs
+++ Assets/Menber/Tataki/AnimationScript/FogAction.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class FogAction : MonoBehaviour
 {
-    //アニメーションが実行されあたら関数を呼び出す
+    // アニメーションが再生中かどうかを示すフラグ
+    private bool animationPlaying = false;
+
+    // アニメーションが実行されたら関数を呼び出す
     public void FogActionFrames()
     {
-        //コルーチンを呼び出す235秒間
-        StartCoroutine(FogActioncontrol(235));
+        if (!animationPlaying)
+        {
+            // コルーチンの呼び出し　アニメーション再生中フラグを設定
+            StartCoroutine(FogActionControl(235));
+        }
     }
-    //コルーチン開始
-    IEnumerator FogActioncontrol()
-    {
 
+    // コルーチン開始
+    IEnumerator FogActionControl(float duration)
+    {
+        // アニメーション再生中フラグをtrue
+        animationPlaying = true;
+
+        // アニメーションの再生待機
+        yield return new WaitForSeconds(duration);
+
+        // アニメーションが終了したら再生中フラグをfalse
+        animationPlaying = false;
     }
 }
-
-//コルーチン使っても他のプログラム実行されるくね？
