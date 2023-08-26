3f54a3ab8f237cc2461029ba795a399b6f08a904
tayano0106 <ttc22c1020035@edu.tech.ac.jp>
2023-08-27 01:51:07 +0900

    ダッシュの仕様追加 CharMoveの一部変更

--- Assets/Menber/Tayano/Scripts/CenterData/CenterDataOfStreamer.cs
+++ Assets/Menber/Tayano/Scripts/CenterData/CenterDataOfStreamer.cs
@@ -8,6 +8,23 @@ public class CenterDataOfStreamer : MonoBehaviour
     public float stHp;
 
     [SerializeField]
-    public float stStamina;
+    public float stBaseSpeed = 5.0f; //ストリーマースピードのベース量
+
+    public float stSpeed;//ストリーマーのスピード
+
+    [SerializeField]
+    public float stDashSpeed = 1.5f; //ストリーマーのダッシュ倍率
+
+    [SerializeField]
+    public float stBaseStamina;//ストリーマースタミナのベース量
+
+    public float stStamina;//ストリーマーのスタミナ
+
+    [SerializeField]
+    public float stStaminaDecrease;//ストリーマーのスタミナの減少量
+
+    [SerializeField]
+    public float stStaminaIncrease;//ストリーマーのスタミナ増加量
+
 
 }
--- Assets/Menber/Tayano/Scripts/Streamer/Dash.cs
+++ Assets/Menber/Tayano/Scripts/Streamer/Dash.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    Animation _animation;
+
+    [SerializeField]
+    CenterDataOfStreamer _centerDataOfStreamer;
+
+    [SerializeField]
+    private bool InputOn = false; //テスト用の入力代替変数
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (_centerDataOfStreamer == null)
+        {
+            _centerDataOfStreamer = GetComponent<CenterDataOfStreamer>();
+        }
 
-    // Update is called once per frame
+        if (_animation == null)
+        {
+            _animation = GetComponent<Animation>();
+        }
+    }
+        // Update is called once per frame
     void Update()
     {
-        
+        dash();
+    }
+
+
+    public void dash()
+    {
+        if (InputOn && _centerDataOfStreamer.stStamina > 0) //入力されている&ストリーマーのスタミナがstStaminaが0より有る状態で以下を行う
+        {
+            _centerDataOfStreamer.stSpeed *= _centerDataOfStreamer.stDashSpeed;//ストリーマーのスピードにダッシュ倍率を掛ける
+
+            _animation.MStSprintAnima();//ダッシュのアニメーションを流す
+
+            _centerDataOfStreamer.stStamina -= _centerDataOfStreamer.stStaminaDecrease;//スタミナをstStaminaDecrease分減少させる
+        }
+        else if(InputOn == false)
+        {
+            _centerDataOfStreamer.stSpeed = _centerDataOfStreamer.stBaseSpeed;//スピードのをベーススピードを代入(初期化)
+
+            if (_centerDataOfStreamer.stStamina < _centerDataOfStreamer.stBaseStamina) //スタミナがベーススタミナより少ない場合以下を行う
+            {
+                _centerDataOfStreamer.stStamina += _centerDataOfStreamer.stStaminaIncrease;//スタミナをスタミナstStaminaIncrease分増加させる
+
+            }
+        }
     }
 }
