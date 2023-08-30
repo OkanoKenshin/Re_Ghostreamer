159a7734937b2852c6e79a0d853eb266dff94aa2
OkanoKenshin <ttc22c1020012@edu.tech.ac.jp>
2023-08-30 15:51:53 +0900

    変更をPush

--- Assets/Menber/Kenshin/Scripts/SceneState/DevicePairingState.cs
+++ Assets/Menber/Kenshin/Scripts/SceneState/DevicePairingState.cs
@@ -2,21 +2,16 @@ using UnityEngine.InputSystem;
 
 public class DevicePairingState : IGameSceneState
 {
-    AssociateWithInputUser _associateWithInputUser = new AssociateWithInputUser();
-
     public void HandleInput(InputAction.CallbackContext context)
     {
 
     }
     public void EnterState()
     {
-        _associateWithInputUser.MWaitPlease();
-        InputSystem.onEvent += _associateWithInputUser.OnDevicePair2Display;
     }
 
     public void ExitState()
     {
-        InputSystem.onEvent -= _associateWithInputUser.OnDevicePair2Display;
 
     }
 }
--- Assets/Menber/Kenshin/Scripts/UserData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class UserData
-{
-    public Display display { get; set; }
-    public string characterID { get; set; } = null;
-}
